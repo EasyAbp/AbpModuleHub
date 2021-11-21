c1b47aa77430d06d51c4bdf046b3c5cf3031f382
real-zony <zony@myzony.com>
2021-11-21 18:13:49 +0800

    fix: Completed the module & module type management controller code.

---
+++ src/EasyAbp.AbpModuleHub.HttpApi/Controllers/ModuleManagementController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyAbp.AbpModuleHub.ModuleManagement;
+using EasyAbp.AbpModuleHub.ModuleManagement.Dtos;
+using Volo.Abp.Application.Dtos;
+
+namespace EasyAbp.AbpModuleHub.Controllers
+{
+    public class ModuleManagementController : AbpModuleHubController, IModuleManagementAppService
+    {
+        private readonly IModuleManagementAppService _moduleManagementAppService;
+
+        public ModuleManagementController(IModuleManagementAppService moduleManagementAppService)
+        {
+            _moduleManagementAppService = moduleManagementAppService;
+        }
+
+        public Task CreateModuleAsync(CreateModuleDto input) => _moduleManagementAppService.CreateModuleAsync(input);
+
+        public Task UpdateModuleAsync(UpdateModuleDto input) => _moduleManagementAppService.UpdateModuleAsync(input);
+
+        public Task DeleteModuleAsync(Guid id) => _moduleManagementAppService.DeleteModuleAsync(id);
+
+        public Task<ModuleDto> GetModuleByIdAsync(Guid id) => _moduleManagementAppService.GetModuleByIdAsync(id);
+
+        public Task<PagedResultDto<ModuleListDto>> GetListAsync(PagedResultRequestDto input) => _moduleManagementAppService.GetListAsync(input);
+    }
+}
