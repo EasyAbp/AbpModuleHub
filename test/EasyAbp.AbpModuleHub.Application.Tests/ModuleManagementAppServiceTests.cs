9cf7faa38f934933a197cbbf545df445f09ccfaf
real-zony <zony@myzony.com>
2021-12-27 10:35:03 +0800

    feat: Added unit test files.

---
+++ test/EasyAbp.AbpModuleHub.Application.Tests/ModuleManagementAppServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using EasyAbp.AbpModuleHub.ModuleManagement;
+using Xunit;
+
+namespace EasyAbp.AbpModuleHub
+{
+    public class ModuleManagementAppServiceTests : AbpModuleHubApplicationTestBase
+    {
+        private readonly IModuleManagementAppService _moduleManagementAppService;
+
+        public ModuleManagementAppServiceTests()
+        {
+            _moduleManagementAppService = GetRequiredService<IModuleManagementAppService>();
+        }
+    }
+}
